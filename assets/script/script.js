1e060755fef7f53f9c97f168c32180727dff488e
Callee84 <carl.g.holm@gmail.com>
2022-09-29 16:40:00 +0000

    adding new page - gallery

--- assets/script/script.js
+++ assets/script/script.js
@@ -45,19 +45,14 @@ window.addEventListener("load", type)
 
 setInterval(countdown, 1000)
 
+/**
+ * script for linking sporify
+ */
 
-// const countDown = new Date("Mars 13, 2023 16:00:00").getTime();
+const APIControler = (function() {
+    const clientId = "";
+    const ClientSecret = "";
 
-// const x = setInterval(function() {
-//     const now = new Date().getTime();
     
-//     const distance = countDown - now;
-
-//     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     const min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//     const sec = Math.floor((distance % (1000 * 60)) / 1000);
-
-//     document.getElementById("time-left").innerHTML = days + "d" + hours +"h" + min + "m" + sec + "s"
-
-// },1000);
+}
+)
